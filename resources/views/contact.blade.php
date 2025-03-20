507311c5034e1e3e037f972355a056f99a2d0784
João Nascimento <Joaoncs21@gmail.com>
2025-03-20 20:13:55 -0300

    views adicionadas

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página do cliente</title>
+</head>
+<body>
+    <h1>Página dedicada ao cliente</h1>
+    <a href="/">voltar para home</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('produtos');
+});
